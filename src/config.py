44d88deb195c8505d8389042e00bd394971612b5
christinakoump <christinakoumpog@gmail.com>
2026-07-01 13:56:32 +0300

    Add configuration constants for paths and model

--- src/config.py
+++ src/config.py
@@ -1,1 +1,7 @@
+ROOT_PATH = "/media/blue/ckoumpo/pcgita/PC-GITA_per_task_44100Hz/"
+RESULTS_PATH = "/media/blue/ckoumpo/pcgita/PCGITA_RESULTS/"
 
+TARGET_SR = 16000
+MAX_SEC = 10
+
+MODEL_NAME = "microsoft/wavlm-base"
